5d4dfa0605b6fdbd9b9dc5ef1bca369bc5e3bb3f
OskarP8 <oskar.peetrimagi@gmail.com>
2026-02-12 21:02:09 +0200

    house

--- scenes/nav.gd
+++ scenes/nav.gd
@@ -3,10 +3,11 @@ extends TileMapLayer
 @onready var objects = $"../Y-Sort/Objects"
 
 func _use_tile_data_runtime_update(coords):
+	if coords in objects.get_used_cells_by_id(0):
+		return true
 	if coords in objects.get_used_cells_by_id(1):
 		return true
-	else:
-		return false
+	return false
 
 func _tile_data_runtime_update(coords, tile_data):
 	tile_data.set_navigation_polygon(0, null)
